ebe9ab7328bc3a93b798b79aa3a0eadf97047f48
Zheng Xu <123100763+UGAZhengXu@users.noreply.github.com>
2023-02-22 19:44:22 -0500

    Update Lab3.py

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -1,9 +1,4 @@
 # Part 1: Task 1
-# Create a test dataset, called messy.ssv (semicolon separated value), 
-# in a new directory, lab3/data consisting of at least all the items above. 
-# The test dataset should consist of at least 10 entries that illustrate all of the above problems 
-# (and some good entries)
-
 # Response: Finshed by created messy.ssv
 
 # Part 1: Task 2
@@ -12,4 +7,6 @@ lines=Data.readlines()
 print(lines,"\n",type(lines))
 
 # Part 1: Task 3
+def funTask1(Var1):
+    return Var1
 
