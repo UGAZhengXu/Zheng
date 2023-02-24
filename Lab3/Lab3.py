ec1b63fec09b340c7cc4932abed8a73957cb86e1
Zheng Xu <123100763+UGAZhengXu@users.noreply.github.com>
2023-02-23 22:50:25 -0500

    Updated Lab3

--- Lab3/Lab3.py
+++ Lab3/Lab3.py
@@ -41,8 +41,8 @@ def funPart1(Var1):
         HightNew = Hight_Ori
         WeightNew = Weight_Ori
     else:
-        HightNew = Hight_Ori*2.54
-        WeightNew= Weight_Ori*0.45359237
+        HightNew = int(Hight_Ori*2.54)
+        WeightNew= int(Weight_Ori*0.45359237)
         tem = 1
     ListRe=[Name_First,Name_Last,Gender_Ori[0],AgeNew,HightNew,WeightNew,Email_Ori,tem]
     return ListRe
@@ -122,4 +122,49 @@ if len(Datalines)-len(FirstName1)>1:
 else:
   print(f'There is 1 person reentry their information')
 # There have some difference among the sequence of these two methods
+# However, all the elements containing is the same
 print(FirstName,FirstName1)
+
+# Part 2: Task 1
+# using traditional method; can also use numpy
+Age_tem=[]
+for i,n in enumerate(Age1):
+    sum=0
+    k=0
+    if n<0:
+        pass
+    else:
+        Age_tem.append(Age1[i])
+        sum=sum+Age1[i]
+        k=k+1
+Age_Ave=sum/k
+lenAgetem=len(Age_tem)
+Agesorted=sorted(Age_tem)
+if lenAgetem%2==0:
+   Age_mid=round((Agesorted[lenAgetem/2]+Agesorted[lenAgetem/2-1])/2)
+else:
+   Age_mid=Agesorted[int((lenAgetem-1)/2)]
+NewAge=Age1[:]
+for i,n in enumerate(NewAge):
+    if n<0:
+        NewAge[i] = Age_mid
+print('The Orginal Age is:',Age1)
+print('The Orginal Modified Age is:',NewAge)
+print(f'The age range is [{Agesorted[0]},{Agesorted[-1]}]')
+
+# Part 2: Task 2
+Hight_Unit=[x/100 for x in Hight1]
+BMI=[]
+for i,w in enumerate(Weight1):
+   BMI.append(w/(Hight_Unit[i]**2))
+print('BMI(kg/m^2):',BMI)
+
+# Part 2: Task 3
+Newfile=open('clean.ssv','w')
+for i,n in enumerate(Age1):
+   temstri=[FirstName1[i],LastName1[i],Gender1[i],Email1[i],str(NewAge[i]),str(Hight1[i]),str(Weight1[i]),str(Issue1[i])]
+   Newfile.writelines([';'.join(temstri),'\n'])
+
+# Part 3: Task 1
+
+
